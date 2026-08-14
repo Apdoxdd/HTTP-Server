b3042c3b7297611d163283e3d0ea4e25bed9d2e5
Abdelrahman <abdelrahmansameh69@gmail.com>
2026-08-14 16:02:48 +0300

    added OpenSSL bundle

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
-#include "../include/httpServer.hpp"
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <iostream>
 
-int main ()
-{
-    httpServer server;
-    server.startup();
+int main() {
+        SSL_library_init();
+            std::cout << "OpenSSL version: " << OpenSSL_version(OPENSSL_VERSION) << std::endl;
+                return 0;
 }

7c516e784fde1d064aed0f70c3864049060e265b
Abdelrahman <abdelrahmansameh69@gmail.com>
2026-08-20 18:22:28 +0300

    more readme fixes

--- benchmark/matplotPY/bigfile.py
+++ benchmark/matplotPY/bigfile.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-# Concurrency levels
+
 c_custom = [100, 500, 1000, 5000, 10000]
 req_sec_custom = [629.97, 647.44, 675.85, 723.36, 825.20]
 
@@ -9,7 +9,7 @@ req_sec_apache = [2133.65, 2013.22, 2065.85, 249.81, 495.45]
 
 plt.figure(figsize=(10, 6))
 
-# Plotting performance curves
+
 plt.plot(
     c_custom,
     req_sec_custom,
@@ -27,7 +27,6 @@ plt.plot(
     linewidth=2,
 )
 
-# Graph styling
 plt.title(
     'Web Server Benchmark: Throughput vs Concurrency (3.73 MB Large'
     ' File)\n(10,000 Requests/test)'
@@ -37,7 +36,7 @@ plt.ylabel('Requests per Second (req/sec)')
 plt.grid(True, linestyle='--', alpha=0.7)
 plt.legend()
 
-# Display and save plot
+
 plt.tight_layout()
 plt.savefig(
     r'c:\Users\EL10_gazy\Pictures\server_bigfile_benchmark.png',
--- benchmark/matplotPY/p99.py
+++ benchmark/matplotPY/p99.py
@@ -1,15 +1,14 @@
 import matplotlib.pyplot as plt
 
-# Concurrency levels
 c_custom = [100, 500, 1000, 5000, 10000]
-p99_custom_ms = [4.8, 27.2, 35.3, 30.5, 794.3]  # converted to ms
+p99_custom_ms = [4.8, 27.2, 35.3, 30.5, 794.3]  
 
 c_apache = [100, 500, 1000, 5000]
 p99_apache_ms = [7.7, 50.4, 254.6, 4581.1]  # converted to ms
 
 plt.figure(figsize=(10, 6))
 
-# Plotting performance curves
+
 plt.plot(
     c_custom,
     p99_custom_ms,
@@ -27,7 +26,7 @@ plt.plot(
     linewidth=2,
 )
 
-# Graph styling
+
 plt.title(
     'Web Server Benchmark: p99 Latency vs Concurrency\n(1 Million'
     ' Requests/test)'
@@ -37,7 +36,7 @@ plt.ylabel('p99 Latency (ms)')
 plt.grid(True, linestyle='--', alpha=0.7)
 plt.legend()
 
-# Display and save plot
+
 plt.tight_layout()
 plt.savefig(
     r'c:\Users\EL10_gazy\Pictures\server_p99_benchmark.png',
